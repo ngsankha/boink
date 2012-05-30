b0f4c35f416c67d6c3b57b53834d5ef53edf9aed
Sankha Narayan Guria <sankha93@gmail.com>
2012-05-30 13:05:29 +0530

    added email config

--- boink.py
+++ boink.py
@@ -3,7 +3,6 @@ def showHelp():
 	print("Usage: boink <option> <parameter>\n")
 	print("--set-name\tSet your name for use with Boink")
 	print("--set-email\tSet your email for use with Boink")
-	print("--init or -i\tInitiate a Boink issue system")
 	print("--add or -a\tAdd an issue")
 	print("--close or -c\tClose an issue")
 	print("--show or -s\tShow all issues\n")
@@ -17,13 +16,26 @@ def setname(name):
 	conffile.write(name)
 	conffile.close()
 	print("Ok! Got your name, " + name + ".")
-	
+
+def setemail(email):
+	path = os.path.expanduser("~/.boink")
+	if not(os.path.exists(path)):
+		os.makedirs(path)
+	conffile = open(path+"/email",'w')
+	conffile.write(email)
+	conffile.close()
+	print("Ok! Thats your email, " + email + ".")
+
 if __name__ == '__main__':
 	import sys, os.path
 	if len(sys.argv) < 2:
 		showHelp()
 	elif sys.argv[1] == "--set-name":
 		setname(sys.argv[2])
+	elif sys.argv[1] == "--set-email":
+		setemail(sys.argv[2])
+	elif sys.argv[1] == "--add" or sys.argv[1] == "-a":
+		add(sys.argv[2])
 	else:
 		print("Unknown option.\n")
 		showHelp()
